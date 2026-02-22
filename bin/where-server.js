b83192009dc6e7d2c5968536363f436b8f48b618
masafumi-sasahara <masafumi@sasahara.io>
2026-02-22 20:32:32 +0900

    update lib/server.js

--- bin/where-server.js
+++ bin/where-server.js
@@ -5,40 +5,65 @@ import path from 'node:path';
 
 import * as where from '../index.js';
 
+const resolve = (module) => {
+  return module.indexOf('#module') === -1 ? import.meta.resolve(module) : module;
+};
+
 // main
 const main = async (dir) => {
 
   const port = process.env.npm_config_port || '5570',
         bind = process.env.npm_config_bind || '0.0.0.0';
 
+  // config
+  const serverConfig = await where.loadConfig(dir);
+
+  const serverAppConfig = Object.entries(serverConfig.app).reduce((o, [k, v]) => (
+    { ...o, [k]: { ...v, importPath: resolve(v.app.module) } }
+  ), {});
+
+  const serverAppMergedConfig = await where.mergeSpec(serverAppConfig, serverConfig.spec);
+
   // server
-  const server = http.createServer(await where.server(dir));
+  const httpServer = http.createServer(
+    await where.createServer(dir, { ...serverConfig, app: serverAppMergedConfig })
+  );
 
   // socket
-  const { socket } = await import('@where-org/where-socket').catch(err => {
+  const socket = await import('@where-org/where-socket').catch(err => {
 
     if (err.code !== 'ERR_MODULE_NOT_FOUND') {
       ;;; where.log({ error: err.stack });
     }
-
-    return { socket: null };
+    return null;
 
   });
 
   if (socket) {
-    const webSocketServer = await socket(dir, server).catch(err => {
+
+    const socketConfig = await socket.loadConfig(dir);
+
+    const socketAppConfig = Object.entries(socketConfig.app).reduce((o, [k, v]) => (
+      { ...o, [k]: { ...v, importPath: resolve(v.app.module) } }
+    ), {});
+
+    const socketAppMergedConfig = await socket.mergeSpec(socketAppConfig, socketConfig.spec);
+
+    const webSocketServer = await socket.createSocket(
+        { ...socketConfig, app: socketAppMergedConfig }, httpServer).catch(err => {
+
       if (!err instanceof AggregateError) {
         throw err;
       }
       ;;; where.log({ error: err.message });
     });
-  }
 
-  server.listen(port, bind, () => {
+  }
+  httpServer.listen(port, bind, () => {
     ;;; where.log(`Listening on ${bind}:${port}`);
   });
 
-  server.on('error', (err) => {
+  httpServer.on('error', (err) => {
 
     const { code, syscall } = err;
 
